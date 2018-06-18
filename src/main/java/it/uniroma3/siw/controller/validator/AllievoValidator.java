7c36203d2981fde07f41354ae5040c2169b0febf
lrnzgiusti <lrnz.justy@gmail.com>
2018-06-18 22:14:59 +0200

    [Lorenzo] 18/06, 22:13 - Sistemati validator.
    Inserito pattern matching sull' email, andrebbe creata una classe per le
    costanti.

--- src/main/java/it/uniroma3/siw/controller/validator/AllievoValidator.java
+++ src/main/java/it/uniroma3/siw/controller/validator/AllievoValidator.java
@@ -1,5 +1,7 @@
 package it.uniroma3.siw.controller.validator;
 
+import java.util.regex.Pattern;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Component;
@@ -9,6 +11,7 @@ import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import it.uniroma3.siw.model.Allievo;
+import it.uniroma3.siw.model.Responsabile;
 
 @Component
 public class AllievoValidator implements Validator
@@ -22,6 +25,16 @@ public class AllievoValidator implements Validator
 	        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "telefono", "required");
 	        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "dataNascita", "required");
 	        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "luogoNascita", "required");
+	        
+	        
+	        Allievo allievo= (Allievo) o;
+			final String EMAIL_PATTERN = 
+					"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+					+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+			//match nel caso in cui venga inserita una mail non valida
+			if(!Pattern.matches(EMAIL_PATTERN, allievo.getEmail()))
+				errors.rejectValue("email", "notemail");
 	    }
 
 	    @Override
